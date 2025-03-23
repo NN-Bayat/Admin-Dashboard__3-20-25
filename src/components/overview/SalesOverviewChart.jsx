be0a0f00863c6abceeadbb407dd317776d5d34b6
NN-Bayat <noorullahbayat1414@gmail.com>
2025-03-23 12:42:46 +0430

    feat(overviewPage): the overview page completed

---
+++ src/components/overview/SalesOverviewChart.jsx
@@ -0,0 +1,67 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { motion } from "framer-motion";
+
+const salesData = [
+  { name: "Jul", sales: 1200 },
+  { name: "Aug", sales: 2200 },
+  { name: "Sep", sales: 5100 },
+  { name: "Oct", sales: 4100 },
+  { name: "Nov", sales: 7200 },
+  { name: "Dec", sales: 9200 },
+  { name: "Jan", sales: 5100 },
+  { name: "Feb", sales: 6900 },
+  { name: "Mar", sales: 8800 },
+  { name: "Apr", sales: 6300 },
+  { name: "May", sales: 7400 },
+  { name: "Jun", sales: 12200 },
+];
+
+import React from "react";
+
+const SalesOverviewChart = () => {
+  return (
+    <motion.div
+      className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-2 pr-3 md:p-6 border border-gray-700"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+    >
+      <h2 className="text-lg font-medium mb-4 text-gray-100">Sales Overview</h2>
+
+      <div className="h-80">
+        <ResponsiveContainer width={"100%"} height={"100%"}>
+          <LineChart data={salesData}>
+            <CartesianGrid strokeDasharray="3 3" stroke="#4B5563" />
+            <XAxis dataKey={"name"} stroke="#9ca3af" />
+            <YAxis stroke="#9ca3af" />
+            <Tooltip
+              contentStyle={{
+                backgroundColor: "rgba(31, 41, 55, 0.8)",
+                borderColor: "#4B5563",
+              }}
+              itemStyle={{ color: "#E5E7EB" }}
+            />
+            <Line
+              type="monotone"
+              dataKey="sales"
+              stroke="#6366F1"
+              strokeWidth={3}
+              dot={{ fill: "#6366F1", strokeWidth: 2, r: 6 }}
+              activeDot={{ r: 6, strokeWidth: 2 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </motion.div>
+  );
+};
+
+export default SalesOverviewChart;
