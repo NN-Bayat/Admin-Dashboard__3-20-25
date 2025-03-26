806cf586d1fa34441d1a57476b635f1a5c068840
NN-Bayat <noorullahbayat1414@gmail.com>
2025-03-26 14:56:18 +0430

    feat(AnalyticsPage):the Analytics page and some changes in other files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Use "/" for local dev and "/Admin-Dashboard__3-20-25/" for GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/Admin-Dashboard__3-20-25/" : "/",
+  // base: process.env.NODE_ENV === "pryoduction" ? "/Admin-Dashboard__3-20-25/" : "/",
+  base: "/Admin-Dashboard__3-20-25",
 });
